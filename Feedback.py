b2b9452ce912d438c1eb09f8c3bf01bf5db334e3
Sujeeth94 <sujeethk34@gmail.com>
2025-11-12 14:56:48 +0530

    Update Feedback.py

--- Feedback.py
+++ Feedback.py
@@ -158,7 +158,9 @@ if any(opt in st.session_state.motivation_factors for opt in ["Other", "Otro", "
 # -------------------------------
 try:
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds_dict = st.secrets["google"]  # Make sure your Secrets key is 'google'
+    creds_dict = st.secrets["google"].copy()
+    # Fix private_key: convert literal "\n" to actual newlines
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
     creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
     gc = gspread.authorize(creds)
 
